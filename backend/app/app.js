19f4ca2aeaefd73ceba73ead2a0508a60a0ecb29
Jean Urena <jeanua8@gmail.com>
2022-10-22 09:41:16 -0400

    Commit message

--- backend/app/app.js
+++ backend/app/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/prueba',function(req,res,next){
-    res.status(200).json({nombre:'jessan'})
+    res.status(200).json({nombre:'jessddan'})
 })
 module.exports = app;
 
